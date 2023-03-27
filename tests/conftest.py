4d9f2dbca83d11eac8451917a27dfae3d0f636bd
Alexander Santalov <vgtrk520@yandex.ru>
2023-03-27 20:37:41 +0300

    add_attach

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ from selene.support.shared import browser
 from utils.base_session import BaseSession
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from utils import attach
 load_dotenv()
+authorization_cookie = None
 
 
 @pytest.fixture(scope='session')
@@ -13,15 +15,16 @@ def demoshop():
     return BaseSession(os.getenv("API_URL"))
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def app(demoshop):
+    global authorization_cookie
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
@@ -34,12 +37,15 @@ def app(demoshop):
     browser.config.base_url = (os.getenv("API_URL"))
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    response = demoshop.post(
-        "login", json={"Email": os.getenv("LOGIN"), "Password": os.getenv("PASSWORD")}, allow_redirects=False)
-    authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
+    if authorization_cookie is None:
+        response = demoshop.post(
+            "login", json={"Email": os.getenv("LOGIN"), "Password": os.getenv("PASSWORD")}, allow_redirects=False)
+        authorization_cookie = response.cookies.get("NOPCOMMERCE.AUTH")
     browser.open("Themes/DefaultClean/Content/images/logo.png")
     browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": authorization_cookie})
-    return browser
+    yield browser
+    attach.add_screenshot(browser)
+    attach.add_video(browser)
 
 
 @pytest.fixture(scope='session')
